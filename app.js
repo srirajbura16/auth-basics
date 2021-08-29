7bc840489af85eeec1474ff1e835aa148b981e67
Sriraj Bura <sriraj.bura16@gmail.com>
2021-08-29 00:01:01 -0400

    implemented bcrypt

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const host = process.env.HOST;
@@ -31,9 +32,15 @@ passport.use(
       if (!user) {
         return done(null, false, { mesage: 'Incorrect Username' });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      });
       return done(null, user);
     });
   })
@@ -62,14 +69,21 @@ app.get('/', (req, res) => res.render('index', { user: req.user }));
 app.get('/sign-up', (req, res) => res.render('sign-up-form'));
 
 app.post('/sign-up', (req, res, next) => {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  }).save((err) => {
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+    // if err, do something
     if (err) {
       return next(err);
     }
-    res.redirect('/');
+    // otherwise, store hashedPassword in DB
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
   });
 });
 
